821ea4aa91b28be05c33dc492c8700fd450c88fd
ludovic <ludovic.mangaj@gmail.com>
2021-03-26 18:39:21 +0100

    changes

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use DateTime; 
 use App\Entity\User;
+use App\Entity\Image;
 use App\Entity\Trick;
+use App\Entity\Video;
 use App\Entity\Comment;
 use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -18,39 +21,40 @@ class TrickController extends AbstractController
     /**
      * @Route("show/trick/{id}", name="trick_show")
      */
-    public function showTrick($id, Comment $comment = null, Request $request = null, EntityManagerInterface $manager): Response
+    //public function show($id, Comment $comment = null, Request $request, EntityManagerInterface $manager): Response
+    public function show(Trick $trick, Request $request, EntityManagerInterface $manager): Response
     {
         //we get the trick 
-        $repoTrick = $this->getDoctrine()->getRepository(Trick::class); 
-        $trick = $repoTrick->find($id); 
-
+        //$repoTrick = $this->getDoctrine()->getRepository(Trick::class); 
+        //$trick = $repoTrick->find($id); 
         //creation of the comment form 
-        if(!$comment) {
+        //if(!$comment) {
             $comment = new Comment(); 
-        }
+       // }
         
         $commentForm = $this->createForm(CommentType::class, $comment); 
 
-            //handling of the form
-            $commentForm->handleRequest($request); 
+        //handling of the form
+        $commentForm->handleRequest($request); 
+        if($commentForm->isSubmitted() && $commentForm->isValid()) {
+            if(!$comment->getId()) {
+                $comment
+                    ->setCreationDate(new DateTime())
+                    ->setTrick($trick)
+                    ->setModificationDate(new DateTime())
+                ; 
+            //we get the logged in user's id
+            $loggedInUserId = $this->getUser()->getId();
 
-            if($commentForm->isSubmitted() && $commentForm->isValid()) {
-                if(!$comment->getId()) {
-                    $comment->setCreationDate(new \DateTime())
-                            ->setTrick($trick)
-                            ; 
-                //we get the logged in user's id
-                $loggedInUserId = $this->getUser()->getId();
-
-                $repoUser = $this->getDoctrine()->getRepository(User::class); 
-                $user = $repoUser->find($loggedInUserId); 
-
-                    $comment->setUser($user); 
-                }
-
-                $manager->persist($comment); 
-                $manager->flush(); 
+            $repoUser = $this->getDoctrine()->getRepository(User::class); 
+            $user = $repoUser->find($loggedInUserId); 
+            $comment->setUser($user); 
             }
+
+            $manager->persist($comment)
+                    ->flush()
+            ; 
+        }
 
         return $this->render('trick/show.html.twig', [
             'trick' => $trick, 
@@ -61,7 +65,7 @@ class TrickController extends AbstractController
     /**
      * @Route("/create/trick", name="trick_create")
      */
-    public function createTrick(Request $request, EntityManagerInterface $manager)
+    public function create(Request $request, EntityManagerInterface $manager)
      {
         $trick = New Trick; 
         
@@ -92,9 +96,9 @@ class TrickController extends AbstractController
     /**
     * @Route("/update/trick/{id}", name="trick_update")
     */
-    public function updateTrick($id) {
-         $repo = $this->getDoctrine()->getRepository(Trick::class); 
-         $trick = $repo->find($id); 
+    public function update(Trick $trick) {
+         //$repo = $this->getDoctrine()->getRepository(Trick::class); 
+         //$trick = $repo->find($id); 
 
         return $this->render('trick/update.html.twig', [
             'trick' => $trick
@@ -112,4 +116,30 @@ class TrickController extends AbstractController
         
         return $this->redirectToRoute('home'); 
     }
+
+    /**
+    * @Route("/delete/video/{id}", name="trick_video_delete")
+    */
+    public function deleteTrickVideo(Video $video): RedirectResponse  
+    {
+        $em = $this->getDoctrine()->getManager(); 
+        $em->remove($video); 
+        $em->flush();
+        
+        return $this->redirectToRoute('home'); 
+    }
+
+    /**
+    * @Route("/delete/image/{id}", name="trick_image_delete")
+    */
+    public function deleteTrickImage(Image $image): RedirectResponse  
+    {
+        $em = $this->getDoctrine()->getManager(); 
+        $em->remove($image); 
+        $em->flush();
+        
+        return $this->redirectToRoute('home'); 
+    }
+
+
 }
--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -23,7 +23,7 @@ class Video
     private $path;
 
     /**
-     * @ORM\ManyToOne(targetEntity=trick::class, inversedBy="videos")
+     * @ORM\ManyToOne(targetEntity=Trick::class, inversedBy="videos")
      * @ORM\JoinColumn(nullable=false)
      */
     private $trick;
@@ -45,12 +45,12 @@ class Video
         return $this;
     }
 
-    public function getTrick(): ?trick
+    public function getTrick(): ?Trick
     {
         return $this->trick;
     }
 
-    public function setTrick(?trick $trick): self
+    public function setTrick(?Trick $trick): self
     {
         $this->trick = $trick;
 
